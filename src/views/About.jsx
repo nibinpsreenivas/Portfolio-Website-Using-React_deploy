3e2bdf0a84b7a4af55643e0e43c24a8c53a8052c
Nibin P Sreenivas <nibinpsreenivas55@gmail.com>
2024-05-10 00:55:26 +0530

    deploy files

---
+++ src/views/About.jsx
@@ -0,0 +1,84 @@
+import React, { useContext } from "react";
+import { techStack } from "../constants";
+import { ThemeContext } from "../themeProvider";
+import { motion } from "framer-motion";
+
+const About = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+
+    
+    <div id="about" className={darkMode === true ? "bg-white" : "bg-gray-900"}>
+      <div className="max-w-7xl mx-auto x-4 sm:px-6 lg:px-8 px-4 md:mt-0 pt-24 pb-12">
+        <h2
+          className={
+            darkMode
+              ? "text-5xl font-bold px-4 md:px-0 text-center text-black"
+              : "text-5xl font-bold px-4 md:px-0 text-center text-white"
+          }
+        >
+          About Me
+        </h2>
+        <div>
+          <motion.div>
+            <h4 className="mt-12 text-3xl font-semibold text-orange-500">
+              Myself
+            </h4>
+            <p
+              className={
+                darkMode
+                  ? "mt-4 text-xl text-justify text-gray-500"
+                  : "mt-4 text-xl text-justify text-white"
+              }
+            >
+              
+As a self-taught web and mobile app developer, I bring a wealth of experience to the table. I thrive on the process of transforming ideas into tangible, high-performing software applications, from inception to execution. My expertise spans from conceptualizing new features to translating wireframes and design flows into efficient, user-centric code. I'm deeply committed to crafting seamless user experiences and prioritize writing code that's not just functional but also reusable and optimized. My passion lies in merging design, technology, and innovation seamlessly, guiding projects from conception to fruition. Presently, I'm honing my skills in backend development, leveraging the latest tools and methodologies to create robust and scalable solutions.
+            </p>
+          </motion.div>
+          <motion.div
+          >
+            <h4 className="mt-12 text-3xl font-semibold text-orange-500">
+              Technologies and Tools
+            </h4>
+            <p
+              className={
+                darkMode
+                  ? "mt-4 text-xl text-justify text-gray-500"
+                  : "mt-4 text-xl text-justify text-white"
+              }
+            >
+              Using a combination of cutting-edge technologies and reliable
+              open-source software I build user-focused, performant apps and
+              websites for smartphones, tablets, and desktops.
+            </p>
+          </motion.div>
+          <motion.div className="flex flex-wrap mt-8 flex flex-wrap justify-between ">
+            {techStack.map((el, index) => (
+              <motion.div
+                initial="hidden"
+                whileInView={"visible"}
+                variants={{
+                  visible: {
+                    y: 0,
+                    opacity: 1,
+                    transition: {
+                      type: "spring",
+                    },
+                  },
+                  hidden: { opacity: 1, y: 80 },
+                }}
+                className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40"
+              >
+                <img alt="" src={el.link} className="w-12" />
+                <h4 className="text-md ml-4">{el.name}</h4>
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
